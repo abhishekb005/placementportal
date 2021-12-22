0aac0640ff595d93f339fc6addf6822eec3f5aaa
abhishekb005 <92286155+abhishekb005@users.noreply.github.com>
2021-12-22 18:46:53 +0530

    Update Credential.py

--- placementapp/Credential.py
+++ placementapp/Credential.py
@@ -2,13 +2,6 @@ class conf:
     
     def getConfig():
         config = {
-        "apiKey": "AIzaSyBSuGuIEErPN0kj3Va7uvPJiJ_oBN3mYwk",
-        "databaseURL":"https://campusplacementportal-default-rtdb.asia-southeast1.firebasedatabase.app/",
-        "authDomain": "campusplacementportal.firebaseapp.com",
-        "projectId": "campusplacementportal",
-        "storageBucket": "campusplacementportal.appspot.com",
-        "messagingSenderId": "337427571042",
-        "appId": "1:337427571042:web:1a6cd408a268456c65cc3b",
         "measurementId": "G-B9XLMWY491"
         } 
         return config
